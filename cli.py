5496108bdc48751676d5868094430a35c9b54969
Ovi <137784219+0x0v1@users.noreply.github.com>
2025-03-27 10:43:17 +0000

    Create cli.py

---
+++ cli.py
@@ -0,0 +1,4 @@
+import argparse
+from barghest_ward.core.analyzer import run_all_heuristics
+from barghest_ward.core.parser import parse_input_file
+from barghest_ward.core.report import print_report
